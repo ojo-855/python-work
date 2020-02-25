66f5b758058f44e2ff078bc1f07fff0588ac0e1b
nsahsspecial1 <nsahsspecial1@usi.uncommonschools.org>
2020-02-25 15:20:31 -0500

    assignment one

---
+++ variables.py
@@ -0,0 +1,23 @@
+name= "Mayowa"
+age=18
+
+print("My name is" , name , "and I am" , age)
+
+addedAge=1.0
+futureAge=addedAge+age
+
+print("Next Year I will be", futureAge)
+
+randomInt = 5
+randomFloat = 7.2
+randomBool = True
+randomNone = None
+randomString ="sup"
+
+print(type(randomInt))
+print(type(randomFloat))
+print(type(randomBool))
+print(type(randomeNone))
+print(type(randomString))
+
+
